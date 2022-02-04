bbba9536396b1b5867e8d9032099cc3a9d125194
LorenaToraes <92279156+LorenaToraes@users.noreply.github.com>
2022-02-04 19:46:19 -0300

    Update atividadeprogweb2.js

--- atividadeprogweb2.js
+++ atividadeprogweb2.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", inserir)
-let tamanho = 2;
+document.addEventListener("DOMContentLoaded", colocar)
+let tam = 2;
 
-        function inserir(){
+        function colocar(){
 			div = document.createElement("div");
-			div.style.fontSize = `${tamanho}em`;
-			div.innerHTML = `<button onclick="aumentar()">+</button>
-            <button onclick="diminuir()">-</button>
+			div.style.fontSize = `${tam}em`;
+			div.innerHTML = `<button onclick="maior()">+</button>
+            <button onclick="menor()">-</button>
 			`;
 			document.body.prepend(div)
 		}
 
-        function atualização() {
+        function mudar() {
             const h1 = document.querySelector("h1");
-            h1.style.fontSize = `${tamanho}em`;
+            h1.style.fontSize = `${tam}em`;
         }
-        function aumentar() {
-			if (tamanho < 6) {
-				tamanho += 0.5;
-				atualização();
+        function maior() {
+			if (tam < 6) {
+				tam += 0.5;
+				mudar();
 			}
         }
-        function diminuir() {
-            if (tamanho > 1) {
-                tamanho -= 0.5;
-                atualização();
+        function menor() {
+            if (tam > 1) {
+                tam -= 0.5;
+                mudar();
             }
         }
